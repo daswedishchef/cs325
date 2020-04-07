e482bc0f6ae2418358e8c7f1da1d80d015ee307e
Spencer Carlson <carlsosp@flip3.engr.oregonstate.edu>
2020-04-06 18:56:46 -0700

    All done

--- asn1/insertsort.py
+++ asn1/insertsort.py
@@ -1,2 +1,26 @@
-if __name__ == '__main__':
-    print ("insert sort")
+def insertsort(mylist,outputfile):
+    mylist.pop(0)
+    for i in range(1,len(mylist)):
+        temp = mylist[i]
+        itemp = i-1
+        while itemp>=0 and mylist[itemp]>temp:
+            mylist[itemp+1] = mylist[itemp]
+            itemp -= 1
+        mylist[itemp+1] = temp
+    for i in mylist:
+        outputfile.write(i)
+        outputfile.write(" ")
+    outputfile.write("\n")
+
+def main():
+    outputfile = open("insert.out","w")
+    myfile = open("data.txt","r")
+    fileline = myfile.readlines()
+    for line in fileline:
+        mylist = line.split()
+        insertsort(mylist,outputfile)
+    myfile.close()
+    outputfile.close()
+
+if __name__ == "__main__":
+    main()
--- asn1/mergesort.py
+++ asn1/mergesort.py
@@ -1,2 +1,45 @@
+def mergesort(mylist):
+    if len(mylist) >= 2:
+        M = len(mylist) / 2
+        L = mylist[:M]
+        R = mylist[M:]
+        mergesort(L)
+        mergesort(R)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                mylist[k] = L[i]
+                i += 1
+            else:
+                mylist[k] = R[j]
+                j += 1
+            k += 1
+        for l1 in range(i,len(L)):
+            mylist[k] = L[l1]
+            k += 1
+        for r1 in range(j,len(R)):
+            mylist[k] = R[r1]
+            k += 1
+    return mylist
+
+
+
+def main():
+    outputfile = open("merge.out","w")
+    myfile = open("data.txt","r")
+    fileline = myfile.readlines()
+    for line in fileline:
+        mylist = line.split()
+        mylist.pop(0)
+        mylist = mergesort(mylist)
+        for i in mylist:
+            outputfile.write(i)
+            outputfile.write(" ")
+        outputfile.write("\n")
+    myfile.close()
+    outputfile.close()
+
 if __name__ == '__main__':
-    print ("merge sort")
+    main()
